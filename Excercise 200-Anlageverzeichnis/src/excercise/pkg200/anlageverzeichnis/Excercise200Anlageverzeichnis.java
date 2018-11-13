9c8bdb95358f2b50a519236a942e393e983cf788
patlua16 <44263512+patlua16@users.noreply.github.com>
2018-11-13 15:05:01 +0100

    Add files via upload

---
+++ Excercise 200-Anlageverzeichnis/src/excercise/pkg200/anlageverzeichnis/Excercise200Anlageverzeichnis.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excercise.pkg200.anlageverzeichnis;
+
+/**
+ *
+ * @author lukas
+ */
+public class Excercise200Anlageverzeichnis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
